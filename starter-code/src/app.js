0403553584238496126e0f1933c6fc4f8da1f8ea
Ustin Vaskin <ustin.vaskin2011@gmail.com>
2019-11-16 18:13:24 +0000

    Upcoming films

---
+++ starter-code/src/app.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/style.scss'
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      upcomingMovies: []
+    }
+
+
+  }
+  componentDidMount() {
+    fetch(' https://api.themoviedb.org/3/movie/upcoming?api_key=133ebdcecf545565859351534dc13aec&language=en-US&page=1')
+      .then(res => res.json())
+      .then(res => this.setState({ upcomingMovies: res.results }))
+  }
+  render() {
+    if (!this.state) return null
+    console.log(this.state.upcomingMovies)
+    return (
+      <div>
+        <h1>Flixbox</h1>
+        <h2>Make your screen come true</h2>
+        <div className="Upcoming">
+          {this.state.upcomingMovies.map((results, i) => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="UpcomingList">
+              <img key={i} src={`https://image.tmdb.org/t/p/w300/${results.poster_path}`} alt={results.title}></img>
+              <li id="title" key={i}>{results.title}
+              </li>
+              <div id="overview">
+                <li key={i}>{results.overview}
+                </li>
+                <li key={i}>Release date: {results.release_date}
+                </li>
+              </div>
+            </div>
+          ))
+          }
+        </div>
+      </div>//end of parent
+    ) //end of return method
+  } //end of render
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
